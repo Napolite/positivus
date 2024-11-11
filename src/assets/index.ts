818e2e48bbf46db7c07dbcd8fe13d08a2a3188e5
constantine <okonemmanuelmma@gmail.com>
2024-11-12 00:28:01 +0100

    landing

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 import PositivusLogo from "./images/Frame 9.svg";
+import BodyIllustration from "./images/Illustration.svg";
 
-export { PositivusLogo };
+export { PositivusLogo, BodyIllustration };
