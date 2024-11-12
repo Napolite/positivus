12edeae127773770ad954baf7950b0706a3c7714
constantine <okonemmanuelmma@gmail.com>
2024-11-12 13:14:31 +0100

    landing page

--- src/Pages/Landing/landing.tsx
+++ src/Pages/Landing/landing.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+
+import "./landing.css";
 import { BodyIllustration } from "../../assets";
 
 function Landing() {
   return (
-    <div>
-      <div>
-        <div>Navigating the landscape for success</div>
-        <div>
-          Our digital marketing agency helps businesses grow and succeed online
-          through a range of services including SEO, PPC, social media
-          marketing, and content creation.
+    <div className="root-body">
+      <div className="main-left">
+        <div className="main-body-quote">
+          Navigating the
+          <br /> digital landscape <br /> for success
         </div>
-        <div>Book a consultation</div>
+        <div className="body-quote">
+          Our digital marketing agency helps businesses
+          <br />
+          grow and succeed online through a range of services including SEO,
+          PPC, social media marketing, and content creation.
+        </div>
+        <div className="consultation-button">Book a consultation</div>
       </div>
-      <div>
-        <img src={BodyIllustration} />
-      </div>
+      <img src={BodyIllustration} />
     </div>
   );
 }
