21cdc42d2f11b70303fcded35420c7fb387e7eca
constantine <okonemmanuelmma@gmail.com>
2024-11-14 23:28:42 +0100

    link working process page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/Navbar/Navbar";
 import CaseStudy from "./Pages/case-studies/caseStudy.tsx";
 import Landing from "./Pages/Landing/landing";
 import Services from "./Pages/P-Services/services.tsx";
+import WorkingProcess from "./Pages/working-process/workingProcess.tsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Clientele />
       <Services />
       <CaseStudy />
+      <WorkingProcess />
     </>
   );
 }
