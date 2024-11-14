74990544ab9d5995e30053a38e298709911850ce
constantine <okonemmanuelmma@gmail.com>
2024-11-14 23:28:04 +0100

    started working process page

---
+++ src/Pages/working-process/workingProcess.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Title from "../../components/Title/title";
+
+import "./workinProcess.css";
+
+function WorkingProcess() {
+  return (
+    <div className="work-p-main">
+      <Title
+        titleHeader={"Our Working Process"}
+        titleText={"Step-by-Step Guide to Achieving Your Business Goals"}
+      />
+    </div>
+  );
+}
+
+export default WorkingProcess;
